9226054ef5e8a56cab2b2d78d937e4f1f3099994
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-09-26 00:47:25 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        var loader = document.getElementById('loader');
+        var navbar = document.getElementById('navbar');
+        var content = document.getElementById('content');
+        var footer = document.getElementById('footer');
+        
+
+        loader.style.display = 'none';
+        navbar.style.display = 'block'; 
+        content.style.display = 'block'; 
+        footer.style.display = 'block'; 
+    }, 5000); // 5000 milliseconds = 5 seconds
+});
+
+
+
