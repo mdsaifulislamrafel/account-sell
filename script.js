9f5d469323cb700f1a664e0261eae2af07d7310d
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-09-26 15:36:55 +0600

    add section complete

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ window.addEventListener('load', function() {
     // }, 5000); // 5000 milliseconds = 5 seconds
 });
 
-
+window.onload = function() {
+    const modal = document.getElementById('my_modal_2');
+    modal.showModal();
+}
 
 
